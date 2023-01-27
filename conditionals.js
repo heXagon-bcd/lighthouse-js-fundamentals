79b41b8d6eff0b0ca8af5e9489def3127b85f5ca
Shawn He <shawnhe1991@gmail.com>
2023-01-27 01:14:06 -0500

    udpate 1//27/23

---
+++ conditionals.js
@@ -0,0 +1,25 @@
+
+
+const raining = true;
+//const cold = true;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+
+
+
+
+const cold = false;
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}

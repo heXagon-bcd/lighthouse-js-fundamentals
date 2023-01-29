a3744276883c187b3372844b1b4abe23520cf2cc
Shawn He <shawnhe1991@gmail.com>
2023-01-29 16:59:27 -0500

    update

---
+++ chorus-for.js
@@ -0,0 +1,8 @@
+const chorus = "Let's dance!";
+for (let repeat = 0; repeat < 10; repeat++) {
+  if(repeat === 5){
+    console.log("*change key*");
+  } else {
+  console.log(chorus);}
+}
+console.log("Until the sun comes up!");
